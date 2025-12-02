d8ace64ed6e4e85feefdf018a7074c0aa2ff91e4
JeanP2501 <jeanydrogos@gmail.com>
2025-12-02 13:12:39 -0500

    feat: initial commit - Config Server Parte I completa

---
+++ src/main/java/com/bank/configserver/ConfigserverApplication.java
@@ -0,0 +1,19 @@
+package com.bank.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Config Server Application
+ * Centralized configuration management for all microservices
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigserverApplication.class, args);
+	}
+
+}
